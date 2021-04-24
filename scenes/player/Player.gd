cabde4767eb83b9a7582ade2334292d8b99c0d86
FlorianBaba <winnymaru@gmail.com>
2021-04-24 14:03:16 +0200

    Added jumping action to player

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,11 +1,18 @@
 extends KinematicBody2D
 
-class_name Perso
+class_name Player
 
-const CLASS_NAME = 'Perso'
+const CLASS_NAME = 'Player'
 const SPEED = 300
+const JUMP_SPEED = 400
+const GRAVITY = 20
+
 var horizontal_direction = 0
+var vertical_direction = 0
 var velocity := Vector2(0, 0)
+var is_jumping = false;
+
+export var ignore_gravity := false
 
 #### ACCESSORS ####
 
@@ -15,8 +22,15 @@ func get_class() -> String: return CLASS_NAME
 #### BUILT-IN ####
 
 func _physics_process(delta: float) -> void:
+	if !ignore_gravity:
+		velocity.y += GRAVITY
+
 	velocity.x = horizontal_direction * SPEED
-	velocity = move_and_slide(velocity)
+
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+	if is_on_floor():
+		is_jumping = false
 
 #### VIRTUALS ####
 
@@ -30,5 +44,13 @@ func _physics_process(delta: float) -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	horizontal_direction = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	
+	if Input.is_action_just_pressed("jump") && !is_jumping:
+		_jump()
+	
+func _jump() -> void:
+	velocity.y -= JUMP_SPEED
+	is_jumping = true
 
 #### SIGNAL RESPONSES ####
+
