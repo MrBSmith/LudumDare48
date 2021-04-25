7aa1fe791a3bf29e7849f2f924512588c67abbbe
Hugo Baba <Hugobarette@gmail.com>
2021-04-25 15:37:18 +0200

    LevelVariationState added, and Ladder & Crumble scene

--- Autoloads/Events.gd
+++ Autoloads/Events.gd
@@ -1,5 +1,12 @@
 extends EventsBase
 
 # warnings-disable
+
+#### OBJECTS USAGE ####
+
 signal try_opening(obstacle)
 signal open(obstacle)
+
+signal increment_level_variation()
+signal decrement_level_variation()
+signal go_to_level_variation(variation_id)
---
+++ scenes/Level/Variations/LevelVariationState.gd
@@ -0,0 +1,28 @@
+extends StateBase
+class_name LevelVariationState
+
+export var hidden_obj_array : Array = []
+
+#### ACCESSORS ####
+
+func is_class(value: String): return value == "LevelVariationState" or .is_class(value)
+func get_class() -> String: return "LevelVariationState"
+
+
+#### BUILT-IN ####
+
+
+
+#### VIRTUALS ####
+
+
+
+#### LOGIC ####
+
+
+
+#### INPUTS ####
+
+
+
+#### SIGNAL RESPONSES ####
