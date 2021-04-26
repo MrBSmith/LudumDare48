793017178067e5f843e3a482a4c2f3d6b81f4885
Hugo Baba <Hugobarette@gmail.com>
2021-04-26 22:12:54 +0200

    Level transition

--- scenes/InteractiveObjects/ObstacleObj/Door/Door.gd
+++ scenes/InteractiveObjects/ObstacleObj/Door/Door.gd
@@ -38,17 +38,20 @@ func _on_state_changed(new_state_name: String):
 	elif new_state_name == "Idle":
 		$CollisionShape2D.set_disabled(false)
 
+
 func _on_body_entered(body: Node):
 	if body.is_class("Player") && $StatesMachine.get_state_name() == "Idle"\
 		&& is_interactable():
 		$StatesMachine.set_state("Opening")
+
 
 func _on_body_exited(body: Node):
 	if body.is_class("Player") && $StatesMachine.get_state_name() == "Opened"\
 		&& is_interactable():
 		$StatesMachine.set_state("Closing")
 
+
 func _on_exit_area_body_entered(body: Node):
 	if body.is_class("Player") && $StatesMachine.get_state_name() == "Opened"\
 		&& is_interactable():
-		EVENTS.emit_signal("go_to_next_level")
+		EVENTS.emit_signal("next_level_query")
