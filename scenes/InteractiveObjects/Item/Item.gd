83c7212bae6f0bcd07de095695245320962804d3
FlorianBaba <winnymaru@gmail.com>
2021-04-26 12:38:57 +0200

    Implemented glow_up on item when collecting it

--- scenes/InteractiveObjects/Item/Item.gd
+++ scenes/InteractiveObjects/Item/Item.gd
@@ -2,6 +2,9 @@ tool
 extends InteractiveObj
 class_name Item
 
+const GLOW_UP_TIME = 0.45
+const GLOW_DOWN_TIME = 0.2
+
 onready var tween = $Tween
 onready var initial_position = get_position()
 
@@ -11,6 +14,8 @@ var vawing_up : bool = bool(Math.randi_range(0, 1))
 export var vawing_offset := Vector2(0, 3)
 
 export var collected = false setget set_collected, is_collected
+
+signal glow_up_finished(item)
 
 #### ACCESSORS ####
 
@@ -33,7 +38,7 @@ func is_collected() -> bool: return collected
 
 func _ready() -> void:
 	var __ = tween.connect("tween_all_completed", self, "_on_tween_all_completed")
-	
+
 	if !is_collected() && !Engine.editor_hint:
 		_start_waving(vawing_up)
 	
@@ -63,15 +68,26 @@ func _start_waving(up: bool = true):
 	tween.interpolate_property(self, "position", get_position(), 
 		initial_position + (vawing_offset * vawing_sign),
 		1.0, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-
 	tween.start()
+
+func glow_up() -> void:
+	var init_scale = get_scale()
+	var glow_up_scale = Vector2(2, 2);
+	tween.interpolate_property(self, "scale", init_scale, glow_up_scale, GLOW_UP_TIME, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	tween.start()
+	yield(tween, "tween_all_completed")
+
+	tween.interpolate_property(self, "scale", glow_up_scale, init_scale, GLOW_DOWN_TIME, Tween.TRANS_CUBIC, Tween.EASE_IN)
+	tween.start()
+	yield(tween, "tween_all_completed")
+	emit_signal("glow_up_finished", self)
 
 #### INPUTS ####
 
 
 #### SIGNAL RESPONSES ####
 
-func _on_tween_all_completed():
+func _on_tween_all_completed() -> void:
 	if !is_collected():
 		vawing_up = !vawing_up
 		_start_waving(vawing_up)
--- scenes/Inventory/Inventory.gd
+++ scenes/Inventory/Inventory.gd
@@ -5,6 +5,7 @@ const CLASS_NAME = 'Inventory'
 
 onready var item_container = get_node("ItemContainer")
 onready var tween = $Tween
+onready var timer = $Timer
 
 onready var hidden_pos = item_container.get_position()
 onready var visible_pos = hidden_pos + Vector2(0, get_node("ItemContainer/TextureRect").get_size().y)
@@ -39,9 +40,7 @@ func _ready() -> void:
 	__ = EVENTS.connect("recede_interactable", self, "_on_recede_interactable")
 	__ = tween.connect("tween_all_completed", self, "_on_tween_all_completed")
 
-
 #### VIRTUALS ####
-
 
 
 #### LOGIC ####
@@ -59,42 +58,45 @@ func transition(hide: bool, instant: bool = false):
 	else:
 		item_container.set_position(to)
 
-
 #### INPUTS ####
 
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("toggle_HUD"):
 		set_hidden(!is_hidden())
 
-
 #### SIGNAL RESPONSES ####
 
 func _on_collect(item: Item) -> void:
+	if item == last_interactive_obj_encountered: # @TODO remove ?
+		last_interactive_obj_encountered = null
+
 	var new_item: Item = item.duplicate() if item.is_inside_tree() else item
-	new_item.set_position(Vector2.ZERO)
-	new_item.set_hidden(false)
-	new_item.set_spectral(false)
-	new_item.set_collected(true)
-	item_container.add_item(new_item)
+	new_item.connect("glow_up_finished", self, "_glow_up_finished")
+	_show()
+
+	if is_transitioning(): # @TODO and visible
+		yield(tween, "tween_all_completed")
+
+	_add_item(new_item)
+
+	if !new_item.is_ready: yield(new_item, "ready") # waiting for new_item to be ready
+
+	new_item.glow_up()
 
 
 func _on_try_interact(obstable: ObstacleObj) -> void:
-	var needed_item : String = ""
-	var obstacle_type = obstable.get_class()
-	
-	match(obstacle_type):
-		"Chest": needed_item = "Key"
-		"Crumble": needed_item = "Pickaxe"
-	
-	var item : Item = item_container.get_item(needed_item)
+	# first, we check if we have the item needed to interect with the current obstable
+	var needed_item_name : String = ""
+
+	match(obstable.get_class()):
+		"Chest": needed_item_name = "Key"
+		"Crumble": needed_item_name = "Pickaxe"
+	var item : Item = item_container.get_item(needed_item_name)
+
 	if item != null: # interaction succeed
-		if is_transitioning():
-			yield(tween, "tween_all_completed")
-		item.destroy()
-		yield(item, "tree_exited")
-		item_container.refresh_items_display()
+		_remove_item(item)
 		EVENTS.emit_signal("interaction_succeed", obstable)
-	else:	
+	else: # interaction failed
 		EVENTS.emit_signal("interaction_failed", obstable)
 
 func _on_tween_all_completed():
@@ -102,8 +104,38 @@ func _on_tween_all_completed():
 
 func _on_approch_interactable(obj: InteractiveObj):
 	last_interactive_obj_encountered = obj
-	set_hidden(false)
+	_show()
 
 func _on_recede_interactable(obj: InteractiveObj):
 	if obj == last_interactive_obj_encountered:
+		last_interactive_obj_encountered = null
+		_hide()
+
+func _add_item(item: Item) -> void:
+	item.set_position(Vector2.ZERO)
+	item.set_hidden(false)
+	item.set_spectral(false)
+	item.set_collected(true)
+	item_container.add_item(item)
+
+func _remove_item(item: Item) -> void:
+	if is_transitioning():
+		yield(tween, "tween_all_completed")
+		item.destroy()
+		yield(item, "tree_exited")
+		item_container.refresh_items_display()
+
+func _hide() -> void:
+	if timer.is_stopped() && last_interactive_obj_encountered == null:
 		set_hidden(true)
+
+func _show() -> void:
+	set_hidden(false)
+	
+func _glow_up_finished(item: Item) -> void:
+	item.disconnect("glow_up_finished", self, "_glow_up_finished")
+	
+	timer.start()
+	yield(timer, "timeout")
+
+	_hide()
